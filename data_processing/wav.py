d1894f7d1956c95bc0aa47a057fbb22bf137b8d3
Irene Li <mengzeli@Irenes-MBP.wv.cc.cmu.edu>
2018-04-08 13:38:42 -0400

    add a few comments in data processing

--- data_processing/wav.py
+++ data_processing/wav.py
@@ -2,4 +2,12 @@ import scipy
 import scipy.io.wavfile as siow
 
 (rate, data) = siow.read(filename)
+# data could be a 1D array or 2D array, depending on the number of tracks.
+# For simplicity, we always keep just one track.
+
+
+# Use a sliding window of size 1000, step of size 100.
+# Normalization: squeeze all data points into [0, 1].
+# Want to get a normalized data array X of shape (num_examples, time_steps, n_features), 
+# where num_examples=?, time_steps=20, n_features=50.
 siow.write(filename, rate, data)
