9e7f1a498741edf2685f68b54f6383b51a12b52c
DoudM <kaitlyncel@hotmail.com>
2018-04-08 20:09:25 -0400

    add reconstruct function

---
+++ data_processing/reconstruct_ifft.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+# takes in a 2D array and reconstruct to 1D by applying ifft 
+def reconstruct(window):
+	result = []
+	if len(window) == 0: return result
+	block_len = window.shape[0]/2
+	window[:][:] *= 500000
+	for block in window:
+		real_part = block[0:block_len]
+		imag_part = block[block_len:]
+		conv_block = real_part + 1.0j * imag_part
+		res = np.fft.ifft(conv_block)
+		result.append(res)
+	return result
+
