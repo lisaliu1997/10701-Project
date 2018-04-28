45573124cb48aba273998760868ed70609e58c88
mengze-irene <38188332+mengze-irene@users.noreply.github.com>
2018-04-28 14:12:37 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import model
+import lstm_model
 import write_wav
 import numpy as np
 import random
+
 
 time_steps = 20
 n_features = 100
@@ -14,13 +15,13 @@ def get_data():
 
 def main():
 	X, Y = get_data()
-	train_model = model.build_train(X, Y, n_features, n_units)
+	train_model = lstm_model.build_train(X, Y, n_features, n_units)
 	rate = 10000
 	seconds = 0.5
 	n_steps = int(seconds * rate)
 	seed = random.choice(X)
 	seed = np.reshape(seed, (1, seed.shape[0], seed.shape[1]))
-	output = model.predict_sequence(train_model, seed, n_steps, n_features)
+	output = lstm_model.predict_sequence(train_model, seed, n_steps, n_features)
 	write_wav.write2wav(output, rate, "results/1.wav")
 
 if __name__ == "__main__":
