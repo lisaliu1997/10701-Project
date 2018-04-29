cbb9fa28881d354e1fe69293d0d421132524efc3
DoudM <kaitlyncel@hotmail.com>
2018-04-29 13:58:00 -0400

    mp3 reconstruct

--- data_processing/reconstruct_ifft.py
+++ data_processing/reconstruct_ifft.py
@@ -1,4 +1,5 @@
 import numpy as np
+import write_wav
 
 # takes in a 2D array and reconstruct to 1D by applying ifft 
 def reconstruct(window):
@@ -12,5 +13,15 @@ def reconstruct(window):
 		conv_block = real_part + 1.0j * imag_part
 		res = np.fft.ifft(conv_block)
 		result.append(res)
+	result = np.array(result)
+	result = np.real(result.flatten())
 	return result
+
+conv_arr = np.load('x_dir/a_x.npy')
+(a, b, c) = conv_arr.shape
+new_conv_arr = conv_arr.reshape(a*b, c)
+rate = 10000
+audio_data = reconstruct(new_conv_arr)
+siow.write(audio_data, 1000, data)
+write_wav.write2wav(new_conv_arr, rate, "results/1.wav")
 
